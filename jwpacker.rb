610ae2769fedfc6c80e9ada5b38e9b0f4d70e8de
Nicholas Audo <naudo@naudo.de>
2013-03-28 16:11:29 -0700

    Don't attempt to open already packed images

--- jwpacker.rb
+++ jwpacker.rb
@@ -13,11 +13,16 @@ parsed.xpath("//components/component/@name").each do |component|
   component.xpath("//element/@src").each do |element|
     file_path = File.join( File.dirname(file),"#{component_name}/#{element.value}")
     begin
-      image_file = File.read(file_path)
-      data = ActiveSupport::Base64.encode64(image_file).gsub("\n", '')
-      data_uri_string = "data:image/png;base64,#{data}"
-      element.value = data_uri_string
+      data_uri_base_string = "data:image/png;base64,"
 
+      if element.value.slice(0,data_uri_base_string.length) != data_uri_base_string
+        image_file = File.read(file_path)
+        data = ActiveSupport::Base64.encode64(image_file).gsub("\n", '')
+        data_uri_string = "#{data_uri_string}#{data}"
+        element.value = data_uri_string
+      else
+        puts "Image already packed"
+      end
     rescue
       puts "No File for #{file_path}"
     end
