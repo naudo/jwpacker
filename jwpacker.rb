16c5eda35c6848bc27ba6fa4f49047892445b753
Nicholas Audo <naudo@naudo.de>
2013-03-28 16:51:23 -0700

    add output when packing a file

--- jwpacker.rb
+++ jwpacker.rb
@@ -4,7 +4,6 @@ require "active_support"
 file = File.open(ARGV[0])
 
 parsed = Nokogiri.XML(file)
-puts Nokogiri.XML(file).inspect
 
 parsed.xpath("//components/component/@name").each do |component|
   component_name = component.value
@@ -12,16 +11,18 @@ parsed.xpath("//components/component/@name").each do |component|
 
   component.xpath("//element/@src").each do |element|
     file_path = File.join( File.dirname(file),"#{component_name}/#{element.value}")
+    data_uri_base_string = "data:image/png;base64,"
+
     begin
-      data_uri_base_string = "data:image/png;base64,"
 
       if element.value.slice(0,data_uri_base_string.length) != data_uri_base_string
         image_file = File.read(file_path)
         data = ActiveSupport::Base64.encode64(image_file).gsub("\n", '')
-        data_uri_string = "#{data_uri_string}#{data}"
+        data_uri_string = "#{data_uri_base_string}#{data}"
+        puts "Packing #{file_path}"
         element.value = data_uri_string
       else
-        puts "Image already packed"
+        #puts "Image already packed"
       end
     rescue
       puts "No File for #{file_path}"
